c46d8e1eccf7ddfb14bd7c9ae52a3270adc71409
Krishnamuti <krishnamuti_2009@hotmail.com>
2020-07-26 10:49:58 -0300

    Contexto e Entidades do Contexto

---
+++ WebAPI/WebAPI/Data/HeroiContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class HeroiContext : DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
+    }
+}
