ba3816f7656ea36f3f2be1ac4b9eb621aede573a
Krishnamuti <krishnamuti_2009@hotmail.com>
2020-07-26 13:48:19 -0300

    String de conexão, Migration e Upadte-Database. Banco criado.

--- WebAPI/WebAPI/Data/HeroiContext.cs
+++ WebAPI/WebAPI/Data/HeroiContext.cs
@@ -1,17 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using WebAPI.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace WebAPI.Data
 {
     public class HeroiContext : DbContext
     {
+        
         public DbSet<Heroi> Herois { get; set; }
         public DbSet<Batalha> Batalhas { get; set; }
         public DbSet<Arma> Armas { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("");
-        }
+        public HeroiContext(DbContextOptions options) : base(options){}
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    //optionsBuilder.UseSqlServer("ConnectionString");        
+        //}
+
     }
 }
