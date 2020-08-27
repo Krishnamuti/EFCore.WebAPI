5770ffcc7862e44fcc92f739df13c4c95b6d8497
Krishnamuti <krishnamuti_2009@hotmail.com>
2020-08-27 19:35:51 -0300

    Controllers Heroi e Batalha, consultas e dados relacionados.

---
+++ WebAPI/WebAPI/Controllers/BatalhaController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFCore.Dominio;
+using EFCore.Repo;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BatalhaController : ControllerBase
+    {
+
+        public readonly HeroiContext _context;
+        public BatalhaController(HeroiContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Batalha
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                return Ok(new Batalha());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex}");
+            }
+        }
+
+        // GET: api/Batalha/5
+        [HttpGet("{id}", Name = "GetBatalha")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Batalha
+        [HttpPost]
+        public ActionResult Post(Batalha model)
+        {
+            try
+            {
+                _context.Batalhas.Add(model);
+                _context.SaveChanges();
+                return Ok("Batalha inserida com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex}");
+            }
+        }
+
+        // PUT: api/Batalha/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, Batalha model)
+        {
+            try
+            {
+                if(_context.Batalhas.AsNoTracking().FirstOrDefault(b => b.Id == id) != null)
+                {
+                    _context.Batalhas.Update(model);
+                    _context.SaveChanges();
+                    return Ok("Batalha alterada com sucesso!");
+                }
+                return Ok("Batalha não encontrada.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex}");
+            }
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var batalha = _context.Batalhas.AsNoTracking().FirstOrDefault(b => b.Id == id);
+                _context.Batalhas.Remove(batalha);
+                _context.SaveChanges();
+                return Ok("Batalha excluída com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex}");
+            }
+        }
+    }
+}
